c867f67519070b495fd817f83bb9806a2b895ac0
andrey <andrey0731@mail.ru>
2022-05-20 23:08:03 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "antd/dist/antd.css";
+import { Layout } from "./styled-components/MainLayout";
+import { Wrapper } from "./styled-components/Wrapper";
+
+function App() {
+  return (
+    <Layout>
+      <Wrapper></Wrapper>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/styled-components/MainLayout.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  background: #e5e5e5;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: wrap;
+`;
